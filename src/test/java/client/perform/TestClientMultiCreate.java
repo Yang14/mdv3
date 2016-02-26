1efd2a98d66badac7a8c5dba27689e33a803ba67
Yang14 <359097729@qq.com>
2016-02-26 18:16:57 +0800

    修改客户端创建测试类中bug

--- src/test/java/client/perform/TestClientMultiCreate.java
+++ src/test/java/client/perform/TestClientMultiCreate.java
@@ -47,16 +47,6 @@ public class TestClientMultiCreate {
     }
 
     @Test
-    public void testClearMd() throws InterruptedException, RemoteException {
-        long start = System.currentTimeMillis();
-        clientService.deleteDir("/");
-        long end = System.currentTimeMillis();
-        logger.info(String.format("delete ok, thread count is %s time: %s",
-                (end - start)));
-    }
-
-
-    @Test
     public void testMultiCreate() throws InterruptedException, RemoteException {
         testMultiCreateDir();
         latchForOps.countDown();
@@ -68,7 +58,7 @@ public class TestClientMultiCreate {
             @Override
             public void run() {
                 try {
-                    buildSubDir(Thread.currentThread().getName());
+                    buildSubDir("/" + Thread.currentThread().getName());
                     latchCreate.countDown();
                 } catch (RemoteException e) {
                     e.printStackTrace();
@@ -84,13 +74,14 @@ public class TestClientMultiCreate {
         logger.info(String.format("create ok, thread count is %s time: %s", threadCount, (end - start)));
         latchForOps.countDown();
     }
+
     public void testMultiCreateFile() throws InterruptedException, RemoteException {
         latchForOps.await();
         Runnable run = new Runnable() {
             @Override
             public void run() {
                 try {
-                    buildSubFile(Thread.currentThread().getName() + "-forFile");
+                    buildSubFile("/" + Thread.currentThread().getName() + "-forFile");
                     latchCreate.countDown();
                 } catch (RemoteException e) {
                     e.printStackTrace();
@@ -106,27 +97,6 @@ public class TestClientMultiCreate {
         logger.info(String.format("find ok, thread count is %s time: %s", threadCount, (end - start)));
     }
 
-    public void testMultiFindFile() throws InterruptedException, RemoteException {
-        latchForOps.await();
-        Runnable run = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    buildSubFile(Thread.currentThread().getName() + "-forFile");
-                    latchCreate.countDown();
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < threadCount; ++i) {
-            new Thread(run, threadNameArray[i]).start();
-        }
-        latchFind.await();
-        long end = System.currentTimeMillis();
-        logger.info(String.format("find ok, thread count is %s time: %s", threadCount, (end - start)));
-    }
     private void buildSubDir(String parentDir) throws RemoteException {
         for (int i = 0; i < count; i++) {
             clientService.createDirMd(parentDir, "dir" + i, getMdAttr("dir" + i, i, true));
@@ -138,30 +108,6 @@ public class TestClientMultiCreate {
             clientService.createFileMd(parentDir, "file" + i, getMdAttr("file" + i, i, false));
         }
     }
-
-    private void findSubFile(String parentDir) throws RemoteException {
-        for (int i = 0; i < count; i++) {
-            clientService.findFileMd(parentDir, "file" + i);
-        }
-    }
-
-    public void buildDirTree(String dir) throws RemoteException {
-        long start = System.currentTimeMillis();
-        String secondDir = dir;
-        for (int i = 0; i < 100; i++) {
-            clientService.createDirMd("/", secondDir + i, getMdAttr(secondDir + i, i, true));
-        }
-        long end = System.currentTimeMillis();
-        logger.info(String.format("time: %s", (end - start)));
-    }
-
-    public void testBuildDirTreePerform() throws RemoteException {
-        String dirName = "bin";
-        for (int i = 0; i < 1; i++) {
-            buildDirTree(dirName + i);
-        }
-    }
-
     private MdAttr getMdAttr(String name, int size, boolean isDir) {
         MdAttr mdAttr = new MdAttr();
         mdAttr.setName(name);
